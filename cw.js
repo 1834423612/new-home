04d369c5df1ae3d321cf1f121c1e12e4ad7562d1
1834423612 <1834423612@qq.com>
2022-11-12 17:09:42 -0500

    update client worker

---
+++ cw.js
@@ -0,0 +1,2 @@
+importScripts('https://lib.baomitu.com/clientworker/2.8.1/dist/cw.js') //请及时替换2.8.1为最新版本号
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/clientworker/2.8.1/dist/cw.js') //请及时替换2.4.0为最新版本号
